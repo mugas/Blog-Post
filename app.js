08e8b1e674ac0a9a3ebd551625c990781de43825
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-06-25 11:08:09 +0300

    testing api

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const getMarsWeather = () => {
     .then((response) => {
       response.data.sol_keys.forEach((sol) => console.log(sol));
       console.log(response.data);
-      console.log(response.data[554].AT.av);
+      //console.log(response.data[554].AT.av);
       console.log(response.data.AT.av);
       console.log(response.data.sol_keys[0]);
     });
@@ -48,7 +48,7 @@ const getMarsWeather = () => {
 
 getMarsWeather();
 
-/* const getSecondData = () => {
+ const getSecondData = () => {
   axios.get("https://images-api.nasa.gov/search?q", {}).then((response) => {
     console.log(response.items);
     console.log(response);
@@ -58,11 +58,11 @@ getMarsWeather();
 const getData = () => {
   axios
     .get(
-      "https://api.nasa.gov/planetary/apod?api_key=JvwvVGPszLUjfaOIrKYEWN01deXWiC6vUUVDle8a"
+      "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
     )
     .then((response) => {
       const img = `<img class="testi" src=${response.data.hdurl}`;
-      /*  console.log(response);
+        console.log(response);
         console.log(img);
         console.log(response.data.hdurl); 
       title.insertAdjacentHTML("beforeend", response.data.title);
@@ -77,9 +77,9 @@ const test = () => {
   axios
     .all([getSecondData(), getData()])
     .then(axios.spread(function (acct, perms) {}));
-}; */
+}; 
 
-/* getBtn.addEventListener("click", getData);
+ getBtn.addEventListener("click", getData);
 secondbtn.addEventListener("click", getSecondData);
 secondbtn.addEventListener("click", test); */
 // secondbtn.addEventListener("click", getImageOfTheDay);
