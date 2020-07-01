0b8fa82041ca77e2837944a493cfb1b423162935
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-07-01 10:44:49 +0300

    dividing mars api to different apis

--- app.js
+++ app.js
@@ -5,28 +5,47 @@ const title = document.querySelector(".title");
 const image = document.querySelector(".image"); */
 const imageOfTheDay = document.querySelector(".daily-image");
 const pictureName = document.querySelector(".picture-name");
-const btn = document.querySelector(".btn");
 const marsWeather = document.querySelector(".mars-weather");
+const marsBtn = document.querySelectorAll(".mars__btn1");
+const marsWeatherToday = document.querySelector(".mars__today-data");
+const marsWeatherTomorrow = document.querySelector(".mars__tomorrow-data");
+const btn = document.querySelectorAll(".mars__btn");
+const genera = axios.get("https://api.nasa.gov/planetary/apod?", {
+  params: {
+    api_key: "DEMO_KEY",
+  },
+});
+
+function test() {
+  btn.forEach(function (eachButton) {
+    eachButton.addEventListener("click", function (e) {
+      console.log(e);
+    });
+  });
+}
 
 // Api to get the Image of the day
 
 const getImageOfTheDay = () => {
-  axios
+  /* axios
     .get("https://api.nasa.gov/planetary/apod?", {
       params: {
         api_key: "DEMO_KEY",
       },
-    })
-    .then((response) => {
+    }) */ genera.then(
+    (response) => {
       imageOfTheDay.insertAdjacentHTML(
         "beforeend",
         `<img src=${response.data.hdurl}>`
       );
       pictureName.insertAdjacentHTML("beforeend", `${response.data.title}`);
-    });
+    }
+  );
 };
 
 getImageOfTheDay();
+
+// Api to get Mars Weather
 
 const getMarsWeather = () => {
   axios
@@ -38,15 +57,17 @@ const getMarsWeather = () => {
       },
     })
     .then((response) => {
-      const data = Object.values(response.data);
-      const data3 = data[0].AT.av;
-      console.log(data3);
+      const weather = Object.values(response.data);
+      const weatherToday = weather[0].AT.av;
+      const weatherTomorrow = weather[1].AT.av;
+      marsWeatherToday.insertAdjacentHTML("beforeend", weatherToday);
+      marsWeatherTomorrow.insertAdjacentHTML("beforeend", weatherTomorrow);
     });
 };
 
 getMarsWeather();
 
-const getSecondData = () => {
+/* const getSecondData = () => {
   axios.get("https://images-api.nasa.gov/search?q", {}).then((response) => {
     console.log(response.items);
     console.log(response);
@@ -73,7 +94,7 @@ const test = () => {
   axios
     .all([getSecondData(), getData()])
     .then(axios.spread(function (acct, perms) {}));
-};
+}; */
 
 /* getBtn.addEventListener("click", getData);
 secondbtn.addEventListener("click", getSecondData);
