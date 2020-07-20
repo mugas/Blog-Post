0985c8e268eebf9299ebaf469b95e34a2a37b10e
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-07-20 11:13:20 +0300

    finnish get request

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const marsBtn = document.querySelectorAll(".mars__btn1");
 const marsWeatherToday = document.querySelector(".mars__today-data");
 const marsWeatherTomorrow = document.querySelector(".mars__tomorrow-data");
 const btnMarsToday = document.getElementById("btn__today");
+const marsTitle = document.querySelector(".mars__title");
 const btnMarsTomorrow = document.getElementById("btn__tomorrow");
+const btnNextDay = document.getElementById("btn__nextDay");
+const marsInfoData = document.querySelector(".mars__info-data");
 const apiPicOfTheDay = axios.get("https://api.nasa.gov/planetary/apod?", {
   params: {
     api_key: "DEMO_KEY",
@@ -46,7 +49,7 @@ const getTodayWeather = () => {
     const weather = Object.values(response.data);
     const weatherToday = weather[0].AT.av;
     marsWeatherToday.insertAdjacentHTML("beforeend", weatherToday);
-    console.log("today");
+    btnMarsToday.onclick = () => false;
   });
 };
 
@@ -60,9 +63,22 @@ const getTomorrowWeather = () => {
 
 const getTodayInfo = () => {
   apiMarsWeather.then((response) => {
-    console.log("todayInfo");
+    const weather = Object.values(response.data);
+    const weatherInTwoDays = weather[2].AT.av;
+    console.log(weatherInTwoDays);
+    marsInfoData.insertAdjacentHTML("beforeend", weatherInTwoDays);
   });
 };
 
-btnMarsToday.addEventListener("click", getTodayWeather);
-btnMarsTomorrow.addEventListener("click", getTomorrowWeather);
+btnMarsToday.addEventListener("click", getTodayWeather, {
+  once: true,
+});
+btnMarsTomorrow.addEventListener("click", getTomorrowWeather, {
+  once: true,
+});
+btnNextDay.addEventListener("click", getTodayInfo, {
+  once: true,
+});
+marsTitle.addEventListener("mouseover", getMarsWeather, {
+  once: true,
+});
