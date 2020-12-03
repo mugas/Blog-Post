d8e86a64b769f1bc4e5839316209e2163d800ecc
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-12-03 06:26:51 +0200

    first files of my express blog post

---
+++ myexpress/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello Worlds');
+});
+
+app.get('/teste', function(req, res) {
+  res.sendFile('index.html');
+});
+
+/* 
+app.get('/home', (req, res) => {
+  res.send('ta bem');
+});
+*/
+app.get('/pug', function(req, res) {
+  res.render('index' /* ,{ title: 'Hey', message: 'Hello There' } */);
+});
+
+// app.set('views', './views');
+
+app.set('view engine', 'pug');
+
+// app.use('/static', express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
